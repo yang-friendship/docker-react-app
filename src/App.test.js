a0089a9aa1464de2feef6504734bfe9458b2a790
yangfriendship <yangfriendship@naver.com>
2021-03-27 22:32:48 +0900

    first commit

---
+++ src/App.test.js
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("test1" , () => {
+  var num = 1;
+
+
+});
+
+test("test2" , () => {
+  var num = 1;
+
+
+});
